643c5910e68ab5f0306102faa6d47378ac107726
alexireland <alexireland@users.noreply.github.com>
2023-09-16 15:02:17 -0400

    first version for INSE6220

---
+++ 123.py
@@ -0,0 +1,12 @@
+def say_hello_in_english():
+    print("Hello, World!")
+
+def say_hello_in_klingon():
+    print("nuqneH")
+
+def main():
+    say_hello_in_english()
+    say_hello_in_klingon()
+
+if __name__ == "__main__":
+    main()
